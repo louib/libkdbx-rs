c3565bae9783f42368914dd1156e63d50a2c79b4
louib <code@louib.net>
2022-05-21 09:46:01 -0400

    First extern block

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,16 @@
 use crate::entry::Entry;
 use crate::group::Group;
 
+// bool open(QSharedPointer<const CompositeKey> key, QString* error = nullptr);
+// bool open(const QString& filePath, QSharedPointer<const CompositeKey> key, QString* error = nullptr);
+// bool save(SaveAction action = Atomic, const QString& backupFilePath = QString(), QString* error = nullptr);
+// bool saveAs(const QString& filePath,
+// SaveAction action = Atomic,
+// const QString& backupFilePath = QString(),
+// QString* error = nullptr);
+// bool extract(QByteArray&, QString* error = nullptr);
+// bool import(const QString& xmlExportPath, QString* error = nullptr);
+//
 pub struct Database {
     entries: Vec<Entry>,
     groups: Vec<Group>,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 #[cxx::bridge]
-mod ffi {}
+mod ffi {
+    unsafe extern "C++" {
+        // TODO change this when we have a git submodule for keepassxc.
+        include!("../keepassxc/src/core/Database.h");
+
+        type Database;
+
+        fn new_database(filePath: &str) -> UniquePtr<Database>;
+    }
+}
 
 fn main() {
     println!("Hello, world!");
