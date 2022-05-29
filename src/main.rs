0fd786af3d74db1d35ed5911ed9e4b44be4c8277
louib <code@louib.net>
2022-05-29 16:13:49 -0400

    Fixing the build?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 #[cxx::bridge]
 mod ffi {
     unsafe extern "C++" {
-        include!("libkdbx-rs/keepassxc/src/core/ModifiableObject.h");
+        //include!("libkdbx-rs/keepassxc/src/core/ModifiableObject.h");
         include!("libkdbx-rs/keepassxc/src/core/Database.h");
 
         type Database;
 
-        fn new_database(filePath: &str) -> UniquePtr<Database>;
+        //fn new_database(filePath: &str) -> UniquePtr<Database>;
     }
 }
 
 fn main() {
-    let database = ffi::new_database("path/to/database.rs");
+    //let database = ffi::new_database("path/to/database.rs");
 }
