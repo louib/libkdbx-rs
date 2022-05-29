6a2350699efab8766003f4a6d4f349625bfaad39
louib <code@louib.net>
2022-05-29 16:22:29 -0400

    Adding what I think are the core classes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ mod ffi {
     unsafe extern "C++" {
         include!("libkdbx-rs/keepassxc/src/core/ModifiableObject.h");
         include!("libkdbx-rs/keepassxc/src/core/Database.h");
+        include!("libkdbx-rs/keepassxc/src/core/Entry.h");
+        include!("libkdbx-rs/keepassxc/src/core/Group.h");
+        include!("libkdbx-rs/keepassxc/src/core/CustomData.h");
+        include!("libkdbx-rs/keepassxc/src/core/EntryAttachments.h");
+        include!("libkdbx-rs/keepassxc/src/core/EntryAttributes.h");
+        include!("libkdbx-rs/keepassxc/src/core/Metadata.h");
+        include!("libkdbx-rs/keepassxc/src/core/TimeInfo.h");
+        // include!("libkdbx-rs/keepassxc/src/core/Merger.h");
+        // Resources.h ???
+        // Tools.h ???
+        // TODO What about Global.h??
 
         type Database;
         type ModifiableObject;
